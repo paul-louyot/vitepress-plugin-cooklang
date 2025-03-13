9713cb2792f3596f645cdc56b0689895f34b283a
Paul Louyot <paul@Pauls-MacBook-Pro.local>
2025-03-13 11:32:15 +0100

    feat: add dataLoader & pathsLoader methods

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,42 @@
-export function myFunction() {
-  console.log("Hello from vitepress-plugin-cooklang!");
+import * as fs from "node:fs";
+import { Recipe } from "@cooklang/cooklang-ts";
+import { glob } from "tinyglobby";
+
+type TransformPathType = (input: string) => string;
+
+export function dataLoader(
+  watchedFiles: string[],
+  transformPath?: TransformPathType,
+) {
+  return watchedFiles.map((file) => {
+    let filePath = file;
+    const source = fs.readFileSync(file, "utf-8");
+    const recipe = new Recipe(source);
+    const metadata = recipe.metadata;
+    if (transformPath) {
+      filePath = transformPath(file);
+    }
+    return { filePath, source, recipe, metadata };
+  });
+}
+
+export async function pathsLoader(
+  pattern: string,
+  transformPath?: TransformPathType,
+) {
+  const files = await glob(pattern, {
+    ignore: ["**/node_modules/**", "**/dist/**"],
+    expandDirectories: false,
+  });
+
+  return files.map((file) => {
+    let filePath = file;
+    const source = fs.readFileSync(file, "utf-8");
+    const recipe = new Recipe(source);
+    const metadata = recipe.metadata;
+    if (transformPath) {
+      filePath = transformPath(file);
+    }
+    return { filePath, source, recipe, metadata };
+  });
 }
